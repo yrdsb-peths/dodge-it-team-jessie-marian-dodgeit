614d3c945e1a3c0fc9987614db1aa11caf1d8548
Super-JSB <lujessie099@gmail.com>
2026-04-21 11:34:41 -0400

    fixed some errors, add apple

--- Apple.java
+++ Apple.java
@@ -1,20 +1,15 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class Apple here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+
 public class Apple extends Actor
 {
-    /**
-     * Act - do whatever the Apple wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    public Apple(){
+        
+    }
     public void act()
     {
         // Add your action code here.
-        move(2);
+        move(-2);
+        
     }
 }
--- MyWorld.java
+++ MyWorld.java
@@ -2,7 +2,7 @@ import greenfoot.*;
 
 public class MyWorld extends World {
     public MyWorld() {
-        super(600, 400, 1);
+        super (600, 400, 1);
         
         Apple apple = new Apple();
         addObject(apple, 300, 100);
