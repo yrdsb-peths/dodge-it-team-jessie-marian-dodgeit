0248ac95c9d5d6fe1f1fe4880809069b39aa12ab
Xiangran217w57 <aha.marianli@gmail.com>
2026-04-21 11:34:48 -0400

    add mouseclicked to hero

--- MyWorld.java
+++ MyWorld.java
@@ -4,7 +4,8 @@ public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
         
-        Apple apple = new Apple();
+        Hero hero = new Hero();
+        addObject(hero, 100, 100);
         
     }
 }
