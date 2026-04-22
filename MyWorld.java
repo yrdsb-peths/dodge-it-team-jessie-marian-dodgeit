ed6a561a4f3f3ef5652c2598bdffb33ea3bb8315
Xiangran217w57 <aha.marianli@gmail.com>
2026-04-22 00:55:37 -0400

    revise

--- MyWorld.java
+++ MyWorld.java
@@ -4,15 +4,15 @@ public class MyWorld extends World {
     public MyWorld() {
         super (600, 400, 1);
         
-<<<<<<< HEAD
+
         Hero hero = new Hero();
         addObject(hero, 100, 100);
         
         Hero hero2 = new Hero();
         addObject(hero, 100, 100);
-=======
+
         Apple apple = new Apple();
         addObject(apple, 300, 100);
->>>>>>> Jessie
+
     }
 }
